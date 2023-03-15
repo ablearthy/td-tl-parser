49cfc23831fcb0841aca5120ae12acf606320679
Able Arthy <ablearthy@gmail.com>
2023-03-15 09:35:05 +0300

    Rename `Parser.schema` -> `Schema.parser`

--- src/main/scala/io/github/ablearthy/tl/parser/package.scala
+++ src/main/scala/io/github/ablearthy/tl/parser/package.scala
@@ -3,5 +3,5 @@ package io.github.ablearthy.tl
 import fastparse._
 
 package object parser {
-  def schemaParser[_: P]: P[Schema] = Parser.schema
+  def schemaParser[_: P]: P[Schema] = Schema.parser
 }
